c830e8a193d6b8b0fbf02db66c0b2e38b6be9c6e
Mea <maevski.ed@gmail.com>
2025-10-30 09:23:36 +0300

    Начал добавлять реальные классы. Сначала для авторизации.

---
+++ app/src/main/java/com/kmx3/compose/domain/usecases/AuthApiUseCase.kt
@@ -0,0 +1,27 @@
+package com.kmx3.compose.domain.usecases
+
+import com.kmx3.compose.domain.irepositories.IUserRepository
+import com.kmx3.compose.domain.models.User
+
+
+class AuthApiUseCase(
+    private val repository: IUserRepository,
+//    private val saveTokenPrefUseCase: SaveTokenPrefUseCase,
+//    private val checkRoleApiUseCase: CheckRoleApiUseCase,
+) {
+    suspend operator fun invoke(user: User): Boolean {
+
+//        val result = repository.auth(user)
+
+//        if (result.isSuccess) {
+//            val token = result.getOrNull()
+//            if (token != null) {
+//                saveTokenPrefUseCase(token.token)
+//
+//                val checkFlag = checkRoleApiUseCase(token)
+//                return checkFlag
+//            }
+//        }
+        return false
+    }
+}
