5b3e3c1b35b5b8b8b51f0886f61ed05664ee3972
Mea <maevski.ed@gmail.com>
2025-11-20 12:06:27 +0300

    изменил deprecated вызовы в DateExtensions.kt

--- app/src/main/java/com/kmx3/compose/common/extensions/DateExtensions.kt
+++ app/src/main/java/com/kmx3/compose/common/extensions/DateExtensions.kt
@@ -1,15 +1,23 @@
 package com.kmx3.compose.common.extensions
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-private val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("EEEE, d MMMM yyyy 'года'", Locale("ru"))
+@RequiresApi(Build.VERSION_CODES.O)
+private val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("EEEE, d MMMM yyyy 'года'",
+    Locale.Builder()
+        .setLanguage("ru")
+        .build()
+)
 
 /**
  * Форматирует дату в русскоязычный формат "День недели, день месяц год"
  * Пример: "Понедельник, 1 января 2024 года"
  */
+@RequiresApi(Build.VERSION_CODES.O)
 fun LocalDate.toRussianDate(): String {
     val formattedDate = this.format(dateFormatter)
     return formattedDate.replaceFirstChar { it.uppercase() }
@@ -18,4 +26,5 @@ fun LocalDate.toRussianDate(): String {
 /**
  * Возвращает текущую дату в русскоязычном формате
  */
+@RequiresApi(Build.VERSION_CODES.O)
 fun getCurrentRussianDate(): String = LocalDate.now().toRussianDate()
